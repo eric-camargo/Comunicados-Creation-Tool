2e397ae3cd20271c231d229d5f2a29b8e4fabae2
Eric Camargo <ericbobcamargo@gmail.com>
2020-10-22 23:13:25 -0300

    Estrutura Principal de Pastas do App Criada

---
+++ src/containers/ComunicadoBuilder.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+import classes from './ComunicadoBuilder.module.css';
+
+class ComunicadoBuilder extends Component {
+  state = {
+    preview: false
+  }
+
+  render () {
+    return (
+      <div className={classes.Test}>
+        <h1>Test</h1>
+      </div>
+    )
+
+  }
+
+
+
+
+
+}
+
+export default ComunicadoBuilder;
